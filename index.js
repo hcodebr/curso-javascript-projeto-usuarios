f3aefc4dc0fcd4fde9fd996573047d486c2181de
porcel1910 <porcel_augusto@hotmail.com>
2021-07-30 13:43:52 -0300

    Primeira aula
    comando  basicos  js

---
+++ index.js
@@ -0,0 +1,40 @@
+/*let cor = "amarelo";
+
+if (cor === "verde") {
+    alert("siga");
+} else if (cor === "amarelo" ) {
+    alert("atenção");
+}else{
+alert("pare");
+
+}
+
+
+let cor = "verde";
+
+switch (cor) {
+   
+    case "verde":
+        alert("siga");        
+        break;
+
+    case "amarelo":
+        alert("atenção");
+        break;
+
+    case "vermelho":
+        alert("pare");
+        break;
+
+    default:
+        alert("não sei");
+
+}
+*/ 
+
+let n = 5 ;
+
+for (let i = 0; i <= 10; i++) {
+   alert(`${i} X ${n} = ${i*n}`);
+
+}
